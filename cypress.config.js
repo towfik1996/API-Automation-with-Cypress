d5c3eacceb4b67332f03fff578e22833094eda63
towfik1996 <towfikislam1996@gmail.com>
2023-07-07 20:08:21 +0600

    upload

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+    reporter: 'cypress-mochawesome-reporter',
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+            require('cypress-mochawesome-reporter/plugin')(on);
+            config.specPattern = [
+                'cypress/e2e/UserLogin.cy.js',
+                'cypress/e2e/GetUserList.cy.js',
+                'cypress/e2e/CreateUser.cy.js',
+                'cypress/e2e/DeleteUser.cy.js'
+            ]
+            return config;
+        },
+    },
+});
